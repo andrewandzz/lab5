564f2e1a43105589730cc8981222dfd30f0ee731
Dashkov <dashkov@lab5.com>
2023-11-27 10:43:54 +0200

    Tie everything up

--- Lab5_2/Program.cs
+++ Lab5_2/Program.cs
@@ -44,7 +44,7 @@ class Program
 
     static void PrintOptions()
     {
-        Console.WriteLine("Виберіть режим калькулятора:");
+        Console.WriteLine("Виберіть режим калькулятора (введіть 1 або 2):");
         Console.WriteLine("1. Стандартний");
         Console.WriteLine("2. Інженерний");
         Console.WriteLine("Або введіть \"exit\", щоб завершити роботу програми:");
@@ -52,12 +52,182 @@ class Program
 
     static void RunStandardMode()
     {
+        AbstractCalculator calculator = new OrdinaryCalculator();
+        calculator.PrintIntro();
 
+        while (true)
+        {
+            calculator.PrintOptions();
+
+            var input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+
+            if (int.TryParse(input, out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        RunAddition(calculator);
+                        continue;
+                    case 2:
+                        RunSubtraction(calculator);
+                        continue;
+                    case 3:
+                        RunMultiplication(calculator);
+                        continue;
+                    case 4:
+                        RunDivision(calculator);
+                        continue;
+                }
+            }
+
+            PrintErrorMessage("Некоректно обрана опція.");
+        }
     }
 
     static void RunAdvancedMode()
     {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        calculator.PrintIntro();
 
+        while (true)
+        {
+            calculator.PrintOptions();
+
+            var input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+
+            if (int.TryParse(input, out int option))
+            {
+                switch (option)
+                {
+                    case 1:
+                        RunAddition(calculator);
+                        continue;
+                    case 2:
+                        RunSubtraction(calculator);
+                        continue;
+                    case 3:
+                        RunMultiplication(calculator);
+                        continue;
+                    case 4:
+                        RunDivision(calculator);
+                        continue;
+                    case 5:
+                        RunByteToKilobyte(calculator);
+                        continue;
+                }
+            }
+
+            PrintErrorMessage("Некоректно обрана опція.");
+        }
+    }
+
+    static void RunAddition(AbstractCalculator calculator)
+    {
+        calculator.Num1 = ReadNumber("Введіть перше число:");
+        calculator.Num2 = ReadNumber("Введіть друге число:");
+
+        double result = calculator.Add();
+
+        Console.WriteLine($"Відповідь: {result}");
+    }
+
+    static void RunSubtraction(AbstractCalculator calculator)
+    {
+        calculator.Num1 = ReadNumber("Введіть перше число:");
+        calculator.Num2 = ReadNumber("Введіть друге число:");
+
+        double result = calculator.Subtract();
+
+        Console.WriteLine($"Відповідь: {result}");
+    }
+
+    static void RunMultiplication(AbstractCalculator calculator)
+    {
+        calculator.Num1 = ReadNumber("Введіть перше число:");
+        calculator.Num2 = ReadNumber("Введіть друге число:");
+
+        double result = calculator.Multiply();
+
+        Console.WriteLine($"Відповідь: {result}");
+    }
+
+    static void RunDivision(AbstractCalculator calculator)
+    {
+        while (true)
+        {
+            try
+            {
+                calculator.Num1 = ReadNumber("Введіть перше число:");
+                calculator.Num2 = ReadNumber("Введіть друге число:");
+
+                double result = calculator.Divide();
+
+                Console.WriteLine($"Відповідь: {result}");
+
+                break;
+            }
+            catch (DivideByZeroException)
+            {
+                PrintErrorMessage("Некоректне введення: спроба ділення на нуль.");
+            }
+        }
+    }
+
+    static void RunByteToKilobyte(AbstractCalculator calculator)
+    {
+        calculator.Num1 = ReadNumberOfBytes("Введіть ціле число байтів в діапазоні від 1 до 10^9:");
+
+        double result = ((IAdvancedCalculator)calculator).ByteToKilobyte();
+
+        Console.WriteLine($"Відповідь: {result}");
+    }
+
+    static double ReadNumber(string message)
+    {
+        while (true)
+        {
+            Console.WriteLine(message);
+
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double number))
+            {
+                return number;
+            }
+            else
+            {
+                PrintErrorMessage("Некоректне введення.");
+            }
+        }
+    }
+
+    private static long ReadNumberOfBytes(string message)
+    {
+        while (true)
+        {
+            Console.WriteLine(message);
+
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out long number) && number >= 1 && number <= 1000000000)
+            {
+                return number;
+            }
+            else
+            {
+                PrintErrorMessage("Некоректне введення.");
+            }
+        }
     }
 
     static void PrintErrorMessage(string message)
